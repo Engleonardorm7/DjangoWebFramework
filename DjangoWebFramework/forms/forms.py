681b6aaf6a7cf15d581a644a0a18438c169e7882
Leonardo <leonardorm7@hotmail.com>
2023-12-14 14:55:00 -0500

    Form created with DB using ModelForm

--- DjangoWebFramework/forms/forms.py
+++ DjangoWebFramework/forms/forms.py
@@ -1,13 +1,8 @@
 from django import forms
+from .models import Logger
 
-SHIFTS=(
-    ('1','Morning'),
-    ('2','Afternoon'),
-    ('3','Evening'),
-)
-
-class InputForm(forms.Form):
-    firs_name=forms.CharField(max_length=200, required=False)
-    last_name=forms.CharField(max_length=200, required=False)
-    shift=forms.ChoiceField(choices=SHIFTS, required=False)
-    time_log=forms.TimeField(required=False)
+class LogForm(forms.ModelForm):
+  
+    class Meta:    
+        model=Logger
+        fields = "__all__"
--- DjangoWebFramework/forms/views.py
+++ DjangoWebFramework/forms/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import LogForm
 
 # Create your views here.
 
-from forms.forms import InputForm
-
 def form_view(request):
-    form=InputForm
+    form= LogForm()
+    if request.method=="POST":
+        form = LogForm(request.POST)
+        if form.is_valid():
+            form.save()
     context={'form':form}
     return render(request, 'home.html', context)
